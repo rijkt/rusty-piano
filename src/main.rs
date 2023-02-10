8b6dd2a375d5b363815039376e45612326057f42
Pascal Ellenberger <p.ellenberger@elderbyte.com>
2023-02-10 15:50:31 +0100

    set prescaler and top

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,24 @@ fn main() -> ! {
     let peripherals = arduino_hal::Peripherals::take().unwrap();
     let timer1 = peripherals.TC1;
     // set wave-form generation mode 15 and Compare Output Mode 3
-    timer1.tccr1a.modify(
-	|_, w| w
+    timer1.tccr1a.write(
+	|w| w
 	    .wgm1().bits(0b11)  // Wave-form generation mode 15: FastPWM, TOP in OCR1A Update of OCR1A at BOTTOM, TOV1 Flag Set on TOP
 	    .com1b().bits(0b11) // Set OC1A on compare match, clear OC1A at BOTTOM (inverting mode)
 	    .com1a().bits(0b11) // Set OC1A on compare match, clear OC1A at BOTTOM (inverting mode)
     );
-    timer1.tccr1b.modify(|_, w| w.wgm1().bits(0b11)); // The other half of wgm mode config
+    timer1.tccr1b.write(|w| w.wgm1().bits(0b11)  // The other half of wgm mode config
+			.cs1().bits(0b101)); // prescaler 1024
+
+    timer1.ocr1a.write(|w| w.bits(125)); // TOP
+
     let pins = arduino_hal::pins!(peripherals);
     let output = pins.d10;
     output.into_output();
-    // TODO: set output compare register to 50 of top -> control duty cycle
-    // TODO: set prescaler
     loop {
+	for top in 0..=125 {
+            timer1.ocr1a.write(|w| w.bits(top));
+	    arduino_hal::delay_ms(20); // control duty cycle
+	}
     }
 }
